3171f1a96367108125df8f8a472f5d48744f2902
Afafrr <mateusz.aftanski@gmail.com>
2023-06-21 16:47:38 +0200

    update aboutme

---
+++ src/components/aboutme.jsx
@@ -0,0 +1,8 @@
+export const AboutMe = () => {
+	return (
+		<div className='px-10 py-5 '>
+			<h1 className='text-2xl font-bold text-titlesC'>O mnie</h1>
+            <p className="m-5">Losrem, ipsum dolor sit amet consectetur adipisicing elit. Minus aut doloribus temporibus reprehenderit incidunt natus dicta at exercitationem tempora repellendus.</p>
+		</div>
+	);
+};
